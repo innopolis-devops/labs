3e56271c70db4efbc982fb2256e45a0b9a3d1f2e
DOCtorActoAntohich <fairy666death@mail.ru>
2022-08-30 21:26:27 +0300

    Allow running already built image

--- run.sh
+++ run.sh
@@ -1,2 +1,1 @@
-docker build -t make_your_time -f ./docker/Dockerfile .
-docker run -d -p 8000:8000 --rm --name make_your_time make_your_time
+docker run -d -p 8000:8000 --rm --name make_your_time doctoractoantohich/make_your_time

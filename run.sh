042926f285a84f17b137def0da1de22cff0c44c5
DOCtorActoAntohich <fairy666death@mail.ru>
2022-08-30 20:50:13 +0300

    Make running/stopping easier

--- run.sh
+++ run.sh
@@ -1,1 +1,2 @@
-uvicorn --host 0.0.0.0 --port 8000 app_python.main:app
+docker build -t make_your_time -f ./docker/Dockerfile .
+docker run -d -p 8000:8000 --rm --name make_your_time make_your_time
---
+++ stop.sh
@@ -0,0 +1,1 @@
+docker stop make_your_time

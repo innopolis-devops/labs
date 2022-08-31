0a3f9bf5e57e2e306dca47f6be37ac27d3cfff3e
Anatolay <nihonvasserman228@gmail.com>
2022-08-31 20:58:35 +0700

    Add solution to lab 1

---
+++ app_python/PYTHON.md
@@ -0,0 +1,14 @@
+# General
+
+1. Use `.gitignore` file to keep avoid unnecessary files from being version-controlled
+2. Use linters and prettyfiers to avoid mistakes during development and help keep code consistent (usually comes with IDEs/easily installable)
+
+# Framework
+While advantages are best described by the [docs](https://fastapi.tiangolo.com/features/), the main reasons for choosing FastApi were
+
+1. It is production-ready
+2. Ease of use, low entry threshold (e.g. compared to Django)
+3. Still has lots of features out of the box:
+    1. Testing tools
+    2. Templates
+ 
---
+++ app_python/app/main.py
@@ -0,0 +1,30 @@
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+from WorldTimeAPI import service
+from datetime import datetime, timedelta
+
+app = FastAPI()
+
+templates = Jinja2Templates(directory="templates")
+time_client = service.Client('timezone')
+
+@app.get("/")
+async def return_time(request: Request):
+    request = {
+        "area": "Europe",
+        "location": "Samara"
+        }
+    response = time_client.get(**request)
+
+    format = "%Y-%m-%dT%H:%M:%S" # "2022-08-30T17:38:09.231036+04:00"
+    current_time = datetime.strptime(response.datetime.split(".")[0], format)
+
+    msc_time = current_time - timedelta(hours=1)
+
+    return templates.TemplateResponse("time.html", {"request": request, "time": format_time(msc_time)})
+
+def format_time(time):
+    components = str(time).split(" ")
+    components.reverse()
+
+    return " ".join(components)

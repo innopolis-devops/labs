cf9c21fff73d567749f80ea9f9918d988a87c8cb
Kirill Ivanov <bragov4ik@gmail.com>
2022-08-31 11:46:09 +0300

    Working rust webapp

--- app_rust/src/main.rs
+++ app_rust/src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use rocket::{ get, launch, routes };
+use chrono::{DateTime, FixedOffset, Local};
+
+
+fn get_time_moscow() -> DateTime<FixedOffset> {
+    let hour = 3600;
+    Local::now().with_timezone(&FixedOffset::east(3*hour))
+}
+
+#[get("/")]
+fn index() -> String {
+    format!("Moscow time: {}", get_time_moscow().format("%H:%M:%S"))
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![index])
 }

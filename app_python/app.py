d69f700c200ce2f76265221f65afe0e075bb81cf
ilya <ilya-kolomin2001@yandex.ru>
2022-09-01 13:18:07 +0300

    feat: app_python web application created on flask

---
+++ app_python/app.py
@@ -0,0 +1,22 @@
+from datetime import datetime
+
+import pytz as pytz
+from flask import Flask
+from jinja2 import Environment, PackageLoader, select_autoescape
+
+env = Environment(
+    loader=PackageLoader("app_python"),
+    autoescape=select_autoescape()
+)
+
+app = Flask(__name__)
+
+
+def get_current_moscow_time() -> str:
+    return datetime.now(pytz.timezone('Europe/Moscow')).strftime("%H:%M:%S")
+
+
+@app.route("/")
+def main_page():
+    template = env.get_template("main_page.html.jinja")
+    return template.render(time=get_current_moscow_time())

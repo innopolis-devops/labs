27044182298ab421de8c35570135d617017f083f
German Vechtomov <d4emonset@gmail.com>
2022-09-03 08:58:29 -0400

    initial

---
+++ app_python/app.py
@@ -0,0 +1,14 @@
+from datetime import datetime
+from flask import Flask
+from pytz import timezone
+
+app = Flask(__name__)
+
+@app.route("/")
+def main():
+  tz_mo = timezone('Europe/Moscow')
+  datetime_mo = datetime.now(tz_mo)
+  return "Moscow time: " + str(datetime_mo.strftime("%H:%M:%S"))
+
+if __name__ == '__main__':
+  app.run()

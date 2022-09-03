d130f64f2f83dab9b3236c732569c40f7501424e
German Vechtomov <d4emonset@gmail.com>
2022-09-03 09:26:50 -0400

    added README

---
+++ app_python/README.md
@@ -0,0 +1,22 @@
+# Python implementation
+
+## Dependencies
+
+```
+python -m venv <venv>
+source <venv>/bin/activate
+pip install -r requirements.txt
+```
+
+## Running
+
+```
+FLASK_APP=app.py FLASK_ENV=productiom FLASK_DEBUG=0 flask run --host=0.0.0.0
+```
+
+## Flake8
+
+```
+flake8 --max-line-length=80
+```
+
--- app_python/app.py
+++ app_python/app.py
@@ -4,11 +4,13 @@ from pytz import timezone
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def main():
-  tz_mo = timezone('Europe/Moscow')
-  datetime_mo = datetime.now(tz_mo)
-  return "Moscow time: " + str(datetime_mo.strftime("%H:%M:%S"))
+    tz_mo = timezone('Europe/Moscow')
+    datetime_mo = datetime.now(tz_mo)
+    return "Moscow time: " + str(datetime_mo.strftime("%H:%M:%S"))
+
 
 if __name__ == '__main__':
-  app.run()
+    app.run()

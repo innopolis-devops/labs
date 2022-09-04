74d0fa7fcaed0639fcce6407eb9711906759def3
AKurmazov <akurmazov2001@gmail.com>
2022-09-05 01:05:08 +0300

    Add lab1

---
+++ lab1/app_rust/src/main.rs
@@ -0,0 +1,27 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use]
+extern crate rocket;
+use rocket::response::content::Html;
+
+#[macro_use]
+extern crate chrono;
+
+mod utils;
+
+#[get("/")]
+fn index() -> Html<String> {
+    let utc_now = chrono::Utc::now();
+    let msc_now = utc_now
+        .with_timezone(
+            &chrono::FixedOffset::east(utils::MSC_TZ_OFFSET))
+        .to_rfc3339();
+
+    let html_response = Html(
+        format!("<h1>{}</h1>", msc_now));
+    return html_response;
+}
+
+fn main() {
+    rocket::ignite().mount("/", routes![index]).launch();
+}

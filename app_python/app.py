98244484c497e89c9441ffbdbb67ed18917d4147
Maxim Kozhinov <maxim.kozhinov@waveaccess.global>
2022-09-04 18:01:58 +0300

    lab1: flask application created

---
+++ app_python/app.py
@@ -0,0 +1,20 @@
+from datetime import datetime
+from flask import Flask, render_template
+import pytz
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def moscow_time():
+    moscow_timezone = pytz.timezone('Europe/Moscow')
+    msc_time = 'Current Moscow Time: ' + current_time(moscow_timezone)
+    return render_template('index.html', moscow_time=msc_time)
+
+
+def current_time(timezone):
+    return datetime.now(timezone).strftime('%H:%M:%S')
+
+
+if __name__ == '__main__':
+    app.run()

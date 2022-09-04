3c6c71ae18ff886781100cbd62f3b31f0ffc0e53
boggda <black_sec@protonmail.com>
2022-09-04 13:58:57 -0400

    refactoring

--- app_python/moscow_time/main.py
+++ app_python/moscow_time/main.py
@@ -1,16 +1,20 @@
-from fastapi import FastAPI, Response, status
+from fastapi import FastAPI, Response
 from fastapi.responses import JSONResponse
 from pydantic import BaseModel
 import requests
 import datetime
 
+
 class MSK_time(BaseModel):
-    msk_time : str
+    msk_time: str
+
 
 class Message(BaseModel):
     message: str
 
+
 app = FastAPI()
+
 
 @app.get("/",
          summary="Get moscow time",
@@ -29,17 +33,24 @@ app = FastAPI()
          )
 async def root(response: Response):
     try:
-        r = requests.get("https://timeapi.io/api/Time/current/zone?timeZone=Europe/Moscow")
-    except:
-        return JSONResponse(status_code=500, content={"message": "Failed to connect to timeapi.io!"})
+        url = "https://timeapi.io/api/Time/current/zone?timeZone=Europe/Moscow"
+        r = requests.get(url)
+    except Exception:
+        return JSONResponse(
+                status_code=500,
+                content={"message": "Failed to connect to timeapi.io!"}
+                )
     if r.status_code == 200:
         time_obj = r.json()
-        time = datetime.datetime(time_obj["year"], 
-                                 time_obj["month"], 
+        time = datetime.datetime(time_obj["year"],
+                                 time_obj["month"],
                                  time_obj["day"],
                                  hour=time_obj["hour"],
                                  minute=time_obj["minute"],
                                  second=time_obj["seconds"])
         return MSK_time(msk_time=time.strftime("%H:%M:%S %d.%m.%Y"))
     else:
-        return JSONResponse(status_code=500, content={"message": "Failed to get time with timeapi.io!"})
+        return JSONResponse(
+                status_code=500,
+                content={"message": "Failed to get time with timeapi.io!"}
+                )

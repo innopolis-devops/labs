992fe170a60ef4378898b8b39d8920d90dcff124
Andrey Khoroshavin <47006007+Andrew174194@users.noreply.github.com>
2022-09-04 20:52:21 +0300

    Python app & docs

---
+++ app_python/main.py
@@ -0,0 +1,17 @@
+from flask import Flask
+app = Flask(__name__)
+
+from datetime import datetime
+from zoneinfo import ZoneInfo
+from sys import version_info
+
+@app.route('/')
+def main():
+    time_msc = datetime.now(ZoneInfo("Europe/Moscow")).time()
+    return "Moscow time: " \
+        + time_msc.isoformat(timespec='seconds') \
+        + "<br><br>(the time is actual for the last webpage load)"
+
+if __name__ == "__main__":
+        app.run() if version_info >= (3, 9) else \
+        print("Error: zoneinfo requires Python 3.9 or newer")

a3894eb78a617541339bec3bc0a82e776aa222dc
grishakostarev <_2001>
2022-09-05 22:15:32 +0300

    lab 1: java project

---
+++ app_java/src/main/java/com/example/app_java/AppJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.app_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppJavaApplication.class, args);
+    }
+
+}

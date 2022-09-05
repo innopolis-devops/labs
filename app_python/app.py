b3b36d7904631fb6676a637019c3540a6d939a1e
grishakostarev <_2001>
2022-09-05 20:29:43 +0300

    lab 1: python project

---
+++ app_python/app.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+from datetime import datetime
+import pytz
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def get_moscow_time() -> str:
+    time = datetime.now(pytz.timezone("Europe/Moscow")).strftime("%H:%M:%S")
+    return render_template("time.html", time=time)

206c7065947635a4cdeb629d745e2597a08cd023
Kunkhozhaev <n.kunkhozhaev@gmail.com>
2022-09-05 14:09:31 +0300

    Moscow timezone task

---
+++ app_python/main_app.py
@@ -0,0 +1,16 @@
+import pytz
+
+from datetime import datetime
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def moscow_time():
+    t_zone = pytz.timezone('Europe/Moscow')
+    time_now = datetime.now(t_zone)  # current date and time in timezone = t_zone
+    formatted_time = time_now.strftime("%H:%M:%S")
+    return render_template("moscow_timezone.html", time=formatted_time)
+
+if __name__ == '__main__':
+    app.run()

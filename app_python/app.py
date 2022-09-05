ff7a8242cc0bba1f5fea8b3259374d1bf5c8f960
Maxim Kozhinov <maxim.kozhinov@waveaccess.global>
2022-09-05 16:12:36 +0300

    lab1: refactoring according to pylint corrections

--- app_python/app.py
+++ app_python/app.py
@@ -1,3 +1,7 @@
+"""
+This file manages requests for the time application
+"""
+
 from datetime import datetime
 from flask import Flask, render_template
 import pytz
@@ -7,12 +11,18 @@ app = Flask(__name__)
 
 @app.route('/')
 def moscow_time():
+    """
+    This method accepts GET-request and returns current time in Moscow
+    """
     moscow_timezone = pytz.timezone('Europe/Moscow')
     msc_time = 'Current Moscow Time: ' + current_time(moscow_timezone)
     return render_template('index.html', moscow_time=msc_time)
 
 
 def current_time(timezone):
+    """
+    method returning current time for provided timezone
+    """
     return datetime.now(timezone).strftime('%H:%M:%S')
 
 

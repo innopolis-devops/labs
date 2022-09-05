9ffbd061443d315fec3adbad117c66b8355c9756
emil-khabibullin <emileyray23@gmail.com>
2022-09-05 13:18:58 +0300

    init

---
+++ app_dart/lib/presentation/presentation.dart
@@ -0,0 +1,1 @@
+export 'my_home_page.dart';
---
+++ app_dart/test/moscow_time_test.dart
@@ -0,0 +1,17 @@
+import 'package:app_dart/application/application.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:timezone/data/latest.dart';
+
+void main() {
+  test('Counter value should be incremented', () async {
+    initializeTimeZones();
+    var before = MoscowTime.moscowTimeFormatted;
+    await Future.delayed(const Duration(seconds: 1));
+    var after = MoscowTime.moscowTimeFormatted;
+
+    var beforeSec = int.parse(before.split(':').last);
+    var afterSec = int.parse(after.split(':').last);
+
+    expect((beforeSec + 1) % 60, afterSec);
+  });
+}

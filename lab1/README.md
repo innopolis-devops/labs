b55d9d73df9bc4051a3cf943d315934bfa04d2b1
Daniil Sinelnik <45969897+S1ngle322@users.noreply.github.com>
2022-09-06 11:04:38 +0300

    chore: homework done

---
+++ lab1/README.md
@@ -0,0 +1,14 @@
+# Web application
+Web server which can return Moscow time on page `/api/moscow_time/`
+
+# Install
+1. Create virtual env: `python3 -m venv venv`
+2. Activate venv: `source venv/bin/activate`
+3. Install dependencies: `pip install -f requirements.txt`
+
+# Run
+1. `./run.sh`
+2. Moscow time can be accessed by `/api/moscow_time/` 
+
+# Test
+`./test.sh`
---
+++ lab1/app_python/app/core/config.py
@@ -0,0 +1,24 @@
+from pydantic import AnyHttpUrl, BaseSettings, validator
+from typing import List, Union
+
+
+class Settings(BaseSettings):
+    API_V1_STR: str = "/api"
+    # BACKEND_CORS_ORIGINS is a JSON-formatted list of origins
+    # e.g: '["http://localhost", "http://localhost:4200", "http://localhost:3000", \
+    # "http://localhost:8080", "http://local.dockertoolbox.tiangolo.com"]'
+    BACKEND_CORS_ORIGINS: List[AnyHttpUrl] = []
+
+    @validator("BACKEND_CORS_ORIGINS", pre=True)
+    def assemble_cors_origins(cls, v: Union[str, List[str]]) -> Union[List[str], str]:
+        if isinstance(v, str) and not v.startswith("["):
+            return [i.strip() for i in v.split(",")]
+        elif isinstance(v, (list, str)):
+            return v
+        raise ValueError(v)
+
+    class Config:
+        case_sensitive = True
+
+
+settings = Settings()

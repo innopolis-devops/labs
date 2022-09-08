507a6e87c74909657f110e16b8ca884aafc39f61
ease-ln <69844629+ease-ln@users.noreply.github.com>
2022-09-08 16:36:11 +0300

    hotfix:0.1 16 line

--- app_python/app.py
+++ app_python/app.py
@@ -13,6 +13,6 @@ def moscow_time():
     response = requests.get(url)
     pattern = "%Y-%m-%dT%H:%M:%S.%f%z"
     if response.status_code == 200:
-        date = datetime.datetime.strptime(response.json()["datetime"], pattern)
+        date = datetime.strptime(response.json()['datetime'], pattern)
         time = f"{date.hour}:{date.minute}:{date.second}"
     return f"Moscow time: {time}"

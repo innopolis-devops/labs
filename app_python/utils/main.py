2a5a1092b7c867d5a3399bf2d5b3e0b4b5f2ea77
Evgeny Afanasyev <afazhenya2001@gmail.com>
2022-09-11 14:22:40 +0300

    feature Dockerfile

--- app_python/utils/main.py
+++ app_python/utils/main.py
@@ -1,6 +1,11 @@
 from datetime import datetime
 import pytz
+import yaml
+
 
 def get_current_time_formatted():
-    tz = pytz.timezone(config.TIMEZONE)
+    with open("config.yml", "r", encoding="utf-8") as f:
+        config = yaml.load(f, Loader=yaml.FullLoader)
+    TZ = config["flask"]["timezone"]
+    tz = pytz.timezone(TZ)
     return datetime.now(tz)

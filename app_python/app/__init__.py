e19a7fcf403e29d14c675b3fc3aa9aac0540afd6
zZzwat4er <valenten2001@mail.ru>
2022-09-13 14:49:54 +0300

    python tests

--- app_python/app/__init__.py
+++ app_python/app/__init__.py
@@ -8,11 +8,3 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return f'{datetime.now(pytz.timezone("Europe/Moscow"))}'
-
-
-def main():
-    app.run(debug=True, port=5000, host='0.0.0.0')
-
-
-if __name__ == '__main__':
-    main()
---
+++ app_python/app/__main__.py
@@ -0,0 +1,8 @@
+from app import app
+
+def main():
+    app.run(debug=True, port=5000, host='0.0.0.0')
+
+
+if __name__ == '__main__':
+    main()

ed83522603051aee58d22039656eaab0d8bb9b69
Kirill Ivanov <bragov4ik@hotmail.com>
2022-09-15 12:25:43 +0300

    Format tests

--- app_python/src/app_test.py
+++ app_python/src/app_test.py
@@ -1,6 +1,7 @@
 from datetime import datetime, timezone
 from app import format_time, hello_world, render_time_page
 
+
 def init_test_data():
     return [
         (
@@ -25,21 +26,24 @@ def init_test_data():
         ),
     ]
 
+
 def test_format_time():
     for (time, tz_name, result) in init_test_data():
         assert format_time(time, tz_name) == result
+
 
 def test_page_render_autoformatted_time():
     for (time, tz_name, result) in init_test_data():
         ft = format_time(time, tz_name)
         assert result in render_time_page(ft)
 
+
 def test_page_render_arbitrary_strings():
     sample_strings = [
         "It's devops time!",
         "Doomsday clock time: 23:58:20",
         format_time(datetime.fromtimestamp(86300, timezone.utc), "Doomsday clock"),
-        ""
+        "",
     ]
     for s in sample_strings:
         assert s in render_time_page(s)

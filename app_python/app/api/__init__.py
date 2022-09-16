8987cf478891b6638d3ceda6a27d73857b79070e
Vladislav Safonov <29694249+ntdesmond@users.noreply.github.com>
2022-09-16 20:55:15 +0300

    Move things around

---
+++ app_python/app/api/__init__.py
@@ -0,0 +1,16 @@
+from datetime import datetime
+
+from fastapi import FastAPI, Depends
+from fastapi.responses import PlainTextResponse
+
+from app.services import TimeService
+
+
+time_service = TimeService.from_ntp("time.google.com")
+
+app = FastAPI()
+
+
+@app.get("/", response_class=PlainTextResponse)
+async def show_time(time: datetime = Depends(time_service.get_real_time)):
+    return time.ctime()

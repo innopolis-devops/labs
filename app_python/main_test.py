f89cb91bebd9d84aa4e3c7c97aba40fad0a73035
Danila Moriakov <d.moriakov@gmail.com>
2022-09-17 17:46:02 +0300

    modify tests

--- app_python/main_test.py
+++ app_python/main_test.py
@@ -4,13 +4,14 @@ from main import app
 
 client = TestClient(app)
 
+
 @pytest.mark.freeze_time("2022-12-31 12:12:12", tz_offset=-3)
-def test_get_time():
-    response = client.get("/")
-    assert response.status_code == 200
-    assert response.text == "31-12-2022 12:12:12 MSC"
-    
-def test_get_time_wrong_endpoint():
-    response = client.get("/time")
-    assert response.status_code == 404
-    
+@pytest.mark.parametrize("path, expected_status, expected_result", [
+    ("/", 200, "31-12-2022 12:12:12 MSC"),
+    ("/time", 404, None)
+])
+def test_get_time(path, expected_status, expected_result):
+    response = client.get(path)
+    assert response.status_code == expected_status
+    if expected_status == 200:
+        assert response.text == expected_result

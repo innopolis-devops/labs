63e6bcf131c07fcd4ba2fb0f2f725396d6e659ea
Kirill Ivanov <bragov4ik@hotmail.com>
2022-09-18 16:41:35 +0300

    Add rust unit tests

--- app_rust/src/main.rs
+++ app_rust/src/main.rs
@@ -6,12 +6,40 @@ fn get_time_moscow() -> DateTime<FixedOffset> {
     Local::now().with_timezone(&FixedOffset::east(3 * hour))
 }
 
+fn render_time_page(timezone_name: String, time: DateTime<FixedOffset>) -> String {
+    format!("{} time: {}", timezone_name, time.format("%H:%M:%S"))
+}
+
 #[get("/")]
 fn index() -> String {
-    format!("Moscow time: {}", get_time_moscow().format("%H:%M:%S"))
+    render_time_page("Moscow".to_owned(), get_time_moscow())
 }
 
 #[launch]
 fn rocket() -> _ {
     rocket::build().mount("/", routes![index])
+}
+
+
+#[cfg(test)]
+mod tests {
+    use rocket::{local::blocking::Client, http::Status};
+    use super::*;
+
+    #[test]
+    fn it_responds() {
+        let rocket = rocket::build().mount("/", routes![index]);
+        let client = Client::tracked(rocket).unwrap();
+        let req = client.get("/");        
+        let response = req.dispatch();
+        assert_eq!(response.status(), Status::Ok);
+    }
+
+    #[test]
+    fn it_renders_correctly() {
+        let dt = DateTime::parse_from_str("1983 Apr 13 12:09:14.274 +0000", "%Y %b %d %H:%M:%S%.3f %z").expect("Should parse");
+        let render = render_time_page("Aboba".to_owned(), dt);
+        assert!(render.contains("Aboba"));
+        assert!(render.contains("12:09:14"));
+    }
 }

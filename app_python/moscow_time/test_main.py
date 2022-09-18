23b654bce85756cf2304ef2de35136770d1c625d
boggda <black_sec@protonmail.com>
2022-09-18 05:05:08 +0300

    refactoring

--- app_python/moscow_time/test_main.py
+++ app_python/moscow_time/test_main.py
@@ -1,5 +1,5 @@
 from fastapi.testclient import TestClient
-import datetime
+from datetime import date, datetime
 
 from .main import app
 
@@ -10,14 +10,15 @@ def test_get_main():
     response = client.get("/")
     assert response.status_code == 200
 
+
 def test_docs():
     response = client.get("/docs")
     assert response.status_code == 200
 
+
 def test_date():
     response = client.get("/")
     msk_time = response.json()['msk_time']
-    today = datetime.date.today()
-    test_date = datetime.datetime.strptime(msk_time.split()[1], '%d.%m.%Y').date()
+    today = date.today()
+    test_date = datetime.strptime(msk_time.split()[1], '%d.%m.%Y').date()
     assert today == test_date
-

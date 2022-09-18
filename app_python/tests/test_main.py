bf00d6c804b9966e365a1b8331fa27c8f63cb66b
Anatolay <nihonvasserman228@gmail.com>
2022-09-18 20:52:33 +0300

    Lab 3

--- app_python/tests/test_main.py
+++ app_python/tests/test_main.py
@@ -8,6 +8,7 @@ def test_routes():
     check_route("/", 200)
     check_route("/aboba", 404)
     check_route("/wrong_path", 404)
+    check_route("/wrong", 404)
 
 
 def check_route(route, expected_status):

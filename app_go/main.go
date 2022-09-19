39b4347b4858426ec5dfd3f32fb9b7f5b6a67fed
ilya <ilya-kolomin2001@yandex.ru>
2022-09-20 02:31:50 +0300

    fix_lab3: fix CI

--- app_go/main.go
+++ app_go/main.go
@@ -8,13 +8,18 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func main() {
-	gin.SetMode(gin.ReleaseMode)
+func SetUpRouter() *gin.Engine{
 	router := gin.Default()
 	loc := time.FixedZone("MSK", 3*60*60)
 	router.GET("/", func(c *gin.Context) {
 		c.String(http.StatusOK, time.Now().In(loc).String())
 	})
+	return router
+}
+
+func main() {
+	gin.SetMode(gin.ReleaseMode)
+	router := SetUpRouter()
 	err := router.Run("0.0.0.0:8080")
 	if err != nil {
 		_ = fmt.Errorf("unexpected error")

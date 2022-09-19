ca0728eb5b81d7ca238f7ee65d8e7def2edece12
grishakostarev <_2001>
2022-09-19 22:14:12 +0300

    lab 3

--- app_python/app.py
+++ app_python/app.py
@@ -2,10 +2,13 @@ from flask import Flask, render_template
 from datetime import datetime
 import pytz
 
-app = Flask(__name__)
 
+def create_app():
+    app = Flask(__name__)
 
-@app.route('/')
-def get_moscow_time() -> str:
-    time = datetime.now(pytz.timezone("Europe/Moscow")).strftime("%H:%M:%S")
-    return render_template("time.html", time=time)
+    @app.route('/')
+    def get_moscow_time() -> str:
+        time = datetime.now(pytz.timezone("Europe/Moscow")).strftime("%H:%M:%S")
+        return render_template("time.html", time=time)
+
+    return app
---
+++ app_python/tests/test_app.py
@@ -0,0 +1,21 @@
+import pytest
+from freezegun import freeze_time
+
+
+def test_get_moscow_time_200(client):
+    response = client.get("/")
+    assert response.status_code == 200
+
+
+def test_get_moscow_time_404(client):
+    response = client.get("/time")
+    assert response.status_code == 404
+
+
+def test_get_moscow_time(client):
+    with freeze_time("2001-09-21 21:00:00", tz_offset=-3):
+        response = client.get("/")
+        assert response.text.find('<p>22:00:00</p>') != -1
+
+# ToDo: Update a PYTHON.md ﬁle and describe your unit tests and best practices that you applied.
+# ToDo: Update a README.md ﬁle, add a "Unit tests" part.

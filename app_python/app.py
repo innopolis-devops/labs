077811f288e1bd93b62b6ea911247174a9b15d8e
Igor M <mporeigor@gmail.com>
2022-09-19 23:48:40 +0300

    ⚡fix app.py

--- app_python/app.py
+++ app_python/app.py
@@ -5,11 +5,12 @@ from flask import Flask, render_template
 
 FILE_NAME = "./files/time.txt"
 
+
 def create_app():
     app = Flask(__name__, template_folder='./templates')
-    timezone_name = os.environ.get('TIMEZONE', 'Europe/Moscow')
+    zone = os.environ.get('TIMEZONE', 'Europe/Moscow')
     time_format = os.environ.get('TIME_FORMAT', '%H:%M:%S')
-    zone = timezone(timezone_name)
+    zone = timezone(zone)
 
     @app.route("/")
     def index():
@@ -19,10 +20,10 @@ def create_app():
             os.makedirs(directory)
         with open(FILE_NAME, "a", encoding='utf-8') as file:
             file.write(str(datetime.now(zone)) + '</br>')
-        return render_template('index.html', timezone_name=timezone_name, time=time)
+        return render_template('index.html', timezone_name=zone, time=time)
     return app
 
 
 if __name__ == "__main__":
-    app=create_app()
+    app = create_app()
     app.run(port=8080, host="0.0.0.0", debug=True)

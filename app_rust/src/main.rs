94dfb474080bf2d66090cc439981c8eb9163af9e
AKurmazov <akurmazov2001@gmail.com>
2022-09-19 22:20:01 +0300

    Add rust tests

--- app_rust/src/main.rs
+++ app_rust/src/main.rs
@@ -7,6 +7,9 @@ use rocket::response::content::Html;
 #[macro_use]
 extern crate chrono;
 
+#[macro_use]
+extern crate assert_matches;
+
 mod utils;
 
 #[get("/")]
@@ -20,6 +23,25 @@ fn index() -> Html<String> {
     Html(format!("<h1>{}</h1>", msc_now))
 }
 
+fn rocket() -> rocket::Rocket {
+    rocket::ignite().mount("/", routes![index])
+}
+
 fn main() {
-    rocket::ignite().mount("/", routes![index]).launch();
+    rocket().launch();
+}
+
+#[cfg(test)]
+mod test {
+    use super::rocket;
+    use rocket::local::Client;
+    use rocket::http::Status;
+
+    #[test]
+    fn index() {
+        let client = Client::new(rocket()).expect("valid rocket instance");
+        let mut response = client.get("/").dispatch();
+        assert_eq!(response.status(), Status::Ok);
+        assert_matches!(response.body_string(), Some(_));
+    }
 }

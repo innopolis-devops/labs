4effaa855da39caeeb6d8fad6bacf0d68f18b00e
Abuzyar Tazetdinov <tazetdinov9925@gmail.com>
2022-09-19 17:43:15 +0300

    fix download-hlint.sh

--- scripts/download-hlint.sh
+++ scripts/download-hlint.sh
@@ -5,17 +5,22 @@ set -o errexit
 
 name="hlint"
 dst="$HOME/.local/bin"
-exe="$dst/$name"
 
 mkdir -p "$dst"
 
 os=linux
-url=$(curl -sSL https://api.github.com/repos/ndmitchell/hlint/releases/latest | jq -r ".assets[] | select(.name | test(\"$os\")) | .browser_download_url")
+api_response=$(curl -sSL https://api.github.com/repos/ndmitchell/$name/releases/latest)
+url=$(echo "$api_response" | jq -r ".assets[] | select(.name | test(\"$os\")) | .browser_download_url")
+
+tag_name=$(echo "$api_response" | jq ".tag_name")
+version=$(echo "$tag_name" | sed 's/v//' | sed 's/"//g')
 
 echo "Downloading $url"
 
-curl -sSL "$url" | gunzip > "$exe.tmp"
-chmod +x "$exe.tmp"
-mv "$exe.tmp" "$exe"
+curl -sSL "$url" > "$name.tar.gz"
+tar -xzf "$name.tar.gz"
 
-echo "Installed $name to $exe"
+chmod +x "$name-$version/$name"
+mv "$name-$version/$name" "$dst"
+
+echo "Installed $name to $dst/$name"

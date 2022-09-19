db39a2bb0880b7c6797aff0f8ee2ef5081e098f6
zZzwat4er <valenten2001@mail.ru>
2022-09-19 16:36:09 +0300

    Update dart tests and Dockerfile

--- app_dart/test/time_test.dart
+++ app_dart/test/time_test.dart
@@ -7,27 +7,27 @@ import 'package:timezone/browser.dart' as tz;
 void main() async {
   await tz.initializeTimeZone();
   final zone = tz.getLocation('Europe/Moscow');
-  for (int i = 0; i < 1000; i++) {
-    testWidgets(
-      'time test',
-      (tester) async {
-        await tester.pumpWidget(
-          const ProviderScope(
-            child: MaterialApp(
-              home: HomeScreen(),
-            ),
+  testWidgets(
+    'time test',
+    (tester) async {
+      await tester.pumpWidget(
+        const ProviderScope(
+          child: MaterialApp(
+            home: HomeScreen(),
           ),
-        );
-        await tester.pump(const Duration(milliseconds: 600));
-        final now = tz.TZDateTime.now(zone);
-        final range = DateTimeRange(
-          start: now.subtract(const Duration(milliseconds: 1100)),
-          end: now.add(const Duration(milliseconds: 1100)),
-        );
-        final time = find.byType(Text).at(1).evaluate().first;
-        expect(find.byType(Text), findsNWidgets(2));
-        expect(find.text('Current Time in Europe/Moscow'), findsOneWidget);
-        final string = (time.widget as Text).data!.split(':');
+        ),
+      );
+      await tester.pump(const Duration(milliseconds: 600));
+      final now = tz.TZDateTime.now(zone);
+      final range = DateTimeRange(
+        start: now.subtract(const Duration(milliseconds: 1100)),
+        end: now.add(const Duration(milliseconds: 1100)),
+      );
+      final time = find.byType(Text).at(1).evaluate().first.widget;
+      expect(find.byType(Text), findsNWidgets(2));
+      expect(find.text('Current Time in Europe/Moscow'), findsOneWidget);
+      if (time is Text) {
+        final string = time.data!.split(':');
         final dataToCompare = DateTime(
           now.year,
           now.month,
@@ -42,7 +42,7 @@ void main() async {
               dataToCompare.isBefore(range.end),
           true,
         );
-      },
-    );
-  }
+      }
+    },
+  );
 }

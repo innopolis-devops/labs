6384c178a6986ab322f1244ba2030a0a17866128
e.khabibulin <emileyray23@gmail.com>
2022-09-19 17:57:06 +0300

    fix lint errors

--- app_python/app.py
+++ app_python/app.py
@@ -1,20 +1,25 @@
-from flask import Flask
+"""Creates a web app to show moscow time"""
+
 from datetime import datetime
+from flask import Flask
 import pytz
 
 app = Flask(__name__)
 
 
-def get_time(format, timezone=pytz.timezone('UTC')):
-    return datetime.now(timezone).strftime(format)
+def get_time(time_format, timezone=pytz.timezone('UTC')):
+    """Returns current time for timezone in needed format"""
+    return datetime.now(timezone).strftime(time_format)
 
 
 def get_moscow_time():
+    """Returns current time in Moscow"""
     timezone = pytz.timezone('Europe/Moscow')
-    format = "%b %d %Y %H:%M:%S"
-    return get_time(format, timezone)
+    time_format = "%b %d %Y %H:%M:%S"
+    return get_time(time_format, timezone)
 
 
 @app.route('/')
 def home():
+    """Main page"""
     return f"The current time in Moscow is {get_moscow_time()}"
--- app_python/test.py
+++ app_python/test.py
@@ -1,15 +1,20 @@
+"""Testing get_time function from the app module"""
+
 import unittest
 from time import sleep
 import app
 
 
 class TestStringMethods(unittest.TestCase):
-    def test_get_time(self):
-        format = "%b %d %Y %H:%M:%S"
+    """The class to test app module functions"""
 
-        before = app.get_time(format)
+    def test_get_time(self):
+        """Testing get_time from the app module"""
+        time_format = "%b %d %Y %H:%M:%S"
+
+        before = app.get_time(time_format)
         sleep(1)
-        after = app.get_time(format)
+        after = app.get_time(time_format)
 
         before_sec = before.split(':')[-1]
         after_sec = after.split(':')[-1]

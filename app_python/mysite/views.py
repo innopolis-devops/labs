ecb914a0af82353074d001dfb5785592aa25a818
Talgat Bektleuov <t.bektleuov@innopolis.university>
2022-09-20 06:18:33 +0300

    added tests, github/workflows

---
+++ app_python/mysite/views.py
@@ -0,0 +1,11 @@
+import datetime
+import time
+from django.http import HttpResponse
+from pytz import timezone
+def index(request):
+    t = time.localtime()
+    moscow = timezone('Europe/Moscow')
+    current_datetime = datetime.datetime.now(moscow)
+    # current_time = time.strftime("%H:%M:%S", t)
+    html = "<html><body><b>Current Date and Time in Moscow:</b> %s</body></html>" % current_datetime.strftime('%Y-%m-%d %H-%M-%S')
+    return HttpResponse(html)

1b8746e771ec8779452850b296f2769bd1e964e0
Danila Danko <br4ch1st0chr0n3@gmail.com>
2022-09-21 20:52:04 +0300

    fix purescript app

--- app_purescript/src/Main.purs
+++ app_purescript/src/Main.purs
@@ -2,14 +2,22 @@ module Main where
 
 import Prelude
 
-import Data.DateTime (Time)
+import Control.Monad.Rec.Class (forever)
+import Data.Array (intercalate)
+import Data.DateTime (Time, hour, minute, second)
+import Data.Enum (fromEnum)
 import Data.Maybe (Maybe(..))
+import Data.Time.Duration (Milliseconds(..))
 import Effect (Effect)
-import Effect.Aff (Aff) as EA
+import Effect.Aff as Aff
+import Effect.Aff.Class (class MonadAff)
+import Effect.Now (nowTime)
+import Halogen (liftEffect)
 import Halogen as H
 import Halogen.Aff as HA
 import Halogen.HTML as HH
 import Halogen.HTML.Properties as HP
+import Halogen.Subscription as HS
 import Halogen.VDom.Driver (runUI)
 
 -- HTML starts here
@@ -21,7 +29,7 @@ main = do
       runUI component unit body
   
 -- #Component
-component :: ∀ a b c. H.Component a b c EA.Aff
+component :: forall query input output m. MonadAff m => H.Component query input output m
 component =
   H.mkComponent
     { initialState
@@ -30,31 +38,64 @@ component =
         H.mkEval
           $ H.defaultEval {
             handleAction = handleAction
-          , initialize = Nothing
+          , initialize = Just Init
           }
     }
   where
     initialState _ = Nothing
-    render = html
-  
-  handleAction :: forall output. Action -> H.HalogenM State Action () output Aff Unit
-  handleAction ac = do
-    case ac of
-      Init -> do
 
+render :: forall m. State -> H.ComponentHTML Action () m
+render = 
+  case _ of
+    Just curTime -> okTimeHtml $ renderTime curTime
+    Nothing -> noTimeHtml
+  -- HH.text ("You have been here for " <> show seconds <> " seconds")
+
+renderTime :: Time -> String
+renderTime curTime = intercalate ":" $ show <$> [h, m, s]
+  where
+    h = fromEnum $ hour curTime :: Int
+    m = fromEnum $ minute curTime :: Int
+    s = fromEnum $ second curTime :: Int
+
+okTimeHtml :: forall (a :: Type). String -> HH.HTML a Action
+okTimeHtml st = HH.div_
+    [ HH.h1_ [ HH.text "Current Moscow time (UTC+3:00):" ]
+    , HH.h2_ [ HH.text $ st ]
+    ]
+
+noTimeHtml :: forall (a :: Type). HH.HTML a Action
+noTimeHtml = HH.div_
+    [ HH.h1_ [ HH.text "Current Moscow time (UTC+3:00) ..." ]
+    ]
+
+-- handleAction :: forall output. Action -> H.HalogenM State Action () output Aff Action
+handleAction :: forall output m. MonadAff m => Action -> H.HalogenM State Action () output m Unit
+handleAction = case _ of
+    Init -> do 
+      _ <- H.subscribe =<< timer Tick
+      pure unit
+    Tick -> 
+      do
+        curTime <- liftEffect $ nowTime
+        H.modify_ $ (\_ -> Just curTime)
+      
 -- TODO auto-reload
 
 type State = Maybe Time
 
+data Action = 
+    Init
+  | Tick
 
+timer :: forall m a. MonadAff m => a -> m (HS.Emitter a)
+timer val = do
+  { emitter, listener } <- H.liftEffect HS.create
+  _ <- H.liftAff $ Aff.forkAff $ forever do
+    Aff.delay $ Milliseconds 800.0
+    H.liftEffect $ HS.notify listener val
+  pure emitter
 
-html :: forall (a :: Type). State -> HH.HTML a Unit
-html st = HH.div_
-    [ HH.h1_ [ HH.text "Current Moscow time (UTC+3:00)" ]
-    , HH.h3_ [ HH.text $ show st ]
-    ]
-
--- End of HTML
 
 attr_ :: forall (a :: Row Type) (b :: Type). String -> String -> HP.IProp a b
 attr_ k v = HP.attr (H.AttrName k) v

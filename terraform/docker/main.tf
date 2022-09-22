70e0fb00db75db453713867357d61626b8a079e5
Kirill Ivanov <bragov4ik@hotmail.com>
2022-09-23 01:18:34 +0300

    Fmt

--- terraform/docker/main.tf
+++ terraform/docker/main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     docker = {
-      source = "kreuzwerker/docker"
+      source  = "kreuzwerker/docker"
       version = "~> 2.13.0"
     }
   }
--- terraform/github/main.tf
+++ terraform/github/main.tf
@@ -10,31 +10,31 @@ terraform {
 
 # Configure the GitHub Provider
 provider "github" {
-    # I'm using `GITHUB_TOKEN` env var
+  # I'm using `GITHUB_TOKEN` env var
 }
 
 # Manage the repository
 resource "github_repository" "devops_fork" {
-  name = var.repository_name
+  name        = var.repository_name
   description = "Devops labs solutions for F22 course at Innopolis University"
 
   visibility = "public"
 
   has_downloads = true
-  has_projects = true
-  has_wiki = true
+  has_projects  = true
+  has_wiki      = true
 }
 
 resource "github_branch_default" "master" {
   repository = github_repository.devops_fork.name
-  branch = "master"
+  branch     = "master"
 }
 
 resource "github_branch_protection" "master" {
   repository_id = github_repository.devops_fork.node_id
 
   pattern = github_repository.devops_fork.default_branch
-  
-  allows_deletions = false
+
+  allows_deletions                = false
   require_conversation_resolution = true
 }

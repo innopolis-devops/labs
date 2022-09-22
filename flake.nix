6c1e2fa64b696ab340a904f1a5505b324b460365
Danila Danko <br4ch1st0chr0n3@gmail.com>
2022-09-22 15:31:34 +0300

    try workflow

--- flake.nix
+++ flake.nix
@@ -1,17 +1,17 @@
 {
   inputs = {
-    inputs.url = "github:br4ch1st0chr0n3/flakes?dir=inputs";
-    nixpkgs.follows = "inputs/nixpkgs";
-    flake-utils.follows = "inputs/flake-utils";
-    my-codium.follows = "inputs/my-codium";
-    nix-vscode-marketplace.follows = "inputs/nix-vscode-marketplace";
+    my-inputs.url = "github:br4ch1st0chr0n3/flakes?dir=inputs";
+    nixpkgs.follows = "my-inputs/nixpkgs";
+    flake-utils.follows = "my-inputs/flake-utils";
+    my-codium.follows = "my-inputs/my-codium";
+    nix-vscode-marketplace.follows = "my-inputs/nix-vscode-marketplace";
   };
   outputs =
     { self
     , flake-utils
     , nixpkgs
     , my-codium
-    , inputs
+    , my-inputs
     , nix-vscode-marketplace
     }:
     flake-utils.lib.eachDefaultSystem (system:
@@ -61,9 +61,11 @@
           };
         };
 
-      tools = toList {
+      tools = (toList {
         inherit (shellTools) nix purescript;
-      };
+      }) ++ (builtins.attrValues {
+        inherit (pkgs) docker;
+      });
 
       codiumWithSettings = pkgs.mkShell {
         buildInputs = [ writeSettings codium ];
@@ -96,14 +98,16 @@
 
   nixConfig = {
     extra-substituters = [
-      "https://nix-community.cachix.org"
-      https://br4ch1st0chr0n3-nix-managed.cachix.org
-      "https://br4ch1st0chr0n3-flakes.cachix.org"
+      https://haskell-language-server.cachix.org
+      https://nix-community.cachix.org
+      https://hydra.iohk.io
+      https://br4ch1st0chr0n3.cachix.org
     ];
     extra-trusted-public-keys = [
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      "br4ch1st0chr0n3-nix-managed.cachix.org-1:sDKsfgu5fCCxNwVhZg+AWeGvbLlEtZoyzkSNKRM/KAo="
-      "br4ch1st0chr0n3-flakes.cachix.org-1:Dyc2yLlRIkdbq8CtfOe24QQhQVduQaezkyV8J9RhuZ8="
+      haskell-language-server.cachix.org-1:juFfHrwkOxqIOZShtC4YC1uT1bBcq2RSvC7OMKx0Nz8=
+      nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=
+      hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ=
+      br4ch1st0chr0n3.cachix.org-1:o1FA93L5vL4LWi+jk2ECFk1L1rDlMoTH21R1FHtSKaU=
     ];
   };
 }

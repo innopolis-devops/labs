68074d7a2d14f0d8f4b70610e76c57f47ab68437
Abuzyar Tazetdinov <tazetdinov9925@gmail.com>
2022-09-24 21:33:55 +0300

    add shellcheck; fix shellcheck errors

--- scripts/download-stylish-haskell.sh
+++ scripts/download-stylish-haskell.sh
@@ -9,16 +9,16 @@ echo Downloading $PACKAGE...
 VERSION_REGEX="v\d+\.\d+\.\d+(\.\d+)?"
 
 RELEASES=$(curl --silent https://github.com/haskell/$PACKAGE/releases)
-RELEASE_URL=https://github.com/$(echo $RELEASES | grep -Po "haskell/$PACKAGE/releases/tag/$VERSION_REGEX" | head -n1)
-VERSION=$(echo $RELEASE_URL | grep -Po "$VERSION_REGEX")
+RELEASE_URL=https://github.com/$(echo "$RELEASES" | grep -Po "haskell/$PACKAGE/releases/tag/$VERSION_REGEX" | head -n1)
+VERSION=$(echo "$RELEASE_URL" | grep -Po "$VERSION_REGEX")
 DOWNLOAD_URL="https://github.com/haskell/$PACKAGE/releases/download/$VERSION/stylish-haskell-$VERSION-linux-x86_64.tar.gz"
 TEMP=$(mktemp --directory .$PACKAGE-XXXXX)
 
 cleanup(){
-    rm -r $TEMP
+    rm -r "$TEMP"
 }
 trap cleanup EXIT
 
-curl --progress-bar --location -o$TEMP/$PACKAGE.tar.gz $DOWNLOAD_URL
-tar -xzf $TEMP/$PACKAGE.tar.gz -C$TEMP
-mv $TEMP/$PACKAGE-$VERSION-linux-x86_64/$PACKAGE .
+curl --progress-bar --location -o"$TEMP/$PACKAGE.tar.gz" "$DOWNLOAD_URL"
+tar -xzf "$TEMP/$PACKAGE.tar.gz" -C"$TEMP"
+mv "$TEMP/$PACKAGE-$VERSION-linux-x86_64/$PACKAGE" .

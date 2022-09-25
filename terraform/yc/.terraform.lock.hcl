7159deec4a820eaa531cd916dcfc0c60d83e34b1
AKurmazov <akurmazov2001@gmail.com>
2022-09-25 18:20:56 +0300

    Add base task

---
+++ terraform/yc/.terraform.lock.hcl
@@ -0,0 +1,9 @@
+# This file is maintained automatically by "terraform init".
+# Manual edits may be lost in future updates.
+
+provider "registry.terraform.io/yandex-cloud/yandex" {
+  version = "0.80.0"
+  hashes = [
+    "h1:altICj3OCtELJplYXrWNNBW6s4/nabh3lbHIK5H/UdM=",
+  ]
+}
---
+++ terraform/yc/main.tf
@@ -0,0 +1,48 @@
+terraform {
+  required_providers {
+    yandex = {
+      source = "yandex-cloud/yandex"
+    }
+  }
+  required_version = ">= 0.13"
+}
+
+provider "yandex" {
+  zone = var.yandex_provider_default_zone
+}
+
+resource "yandex_compute_instance" "vm" {
+  name = var.yandex_compute_instance_name
+  allow_stopping_for_update = true
+
+  resources {
+    cores  = 4
+    memory = 4
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = var.yandex_compute_instance_image_id
+    }
+  }
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.subnet.id
+    nat       = true
+  }
+
+  metadata = {
+    ssh-keys = "ubuntu:${file("~/.ssh/id_rsa_yc.pub")}"
+  }
+}
+
+resource "yandex_vpc_network" "network" {
+  name = "network1"
+}
+
+resource "yandex_vpc_subnet" "subnet" {
+  name           = "subnet"
+  zone           = "ru-central1-a"
+  network_id     = yandex_vpc_network.network.id
+  v4_cidr_blocks = ["192.168.10.0/24"]
+}

93a9624e109fe35aa25af76483ee4a9281ce667a
Polina <pminina@mail.ru>
2022-09-27 00:18:48 +0300

    YandexCloud public ip enabled

--- terraform/yandexcloud/outputs.tf
+++ terraform/yandexcloud/outputs.tf
@@ -2,8 +2,3 @@ output "internal_ip_address_vm_1" {
   description = "Internal IP address of virtual machine"
   value       = yandex_compute_instance.vm-1.network_interface.0.ip_address
 }
-
-output "external_ip_address_vm_1" {
-  description = "External IP address of virtual machine"
-  value       = yandex_compute_instance.vm-1.network_interface.0.nat_ip_address
-}

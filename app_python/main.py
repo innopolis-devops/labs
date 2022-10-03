8539ea7cf2c15fbcbe331eb58a69c79b6bb1ae42
Amir <66668728+Amirka-Kh@users.noreply.github.com>
2022-10-03 19:35:04 +0300

    Add timezone handling by API

--- app_python/main.py
+++ app_python/main.py
@@ -1,34 +1,70 @@
-from datetime import datetime
 import pytz
+from datetime import datetime
 
 import flask
 from flask import Flask, render_template
+from markupsafe import escape
+
 
 app = Flask(__name__)
 
 
-class Clock():
-    def __init__(self, *args):
-        self.some_tz = pytz.timezone(args[0])
+class InvalidTimezoneError(ValueError):
+    def __init__(self, wrong_url: str):
+        self.wrong_url = wrong_url
 
-    def print_time(self, format="%H:%M:%S"):
+    def __str__(self):
+        return f"Our website cannot recognize such timezone: {self.wrong_url}"
+
+
+def set_timezone(some_zone):
+    try:
+        return pytz.timezone(some_zone)
+    except pytz.UnknownTimeZoneError:
+        raise InvalidTimezoneError(some_zone)
+
+
+class Clock:
+    def __init__(self, *args) -> None:
+        self.some_tz = set_timezone(args[0])
+
+    def print_time(self, format="%H:%M:%S") -> None:
         some_datetime = datetime.now(self.some_tz)
         print("{} time:".format(self.some_tz), some_datetime.strftime(format))
 
-    def bring_time(self, format="%H:%M:%S"):
+    def bring_time(self, format="%H:%M:%S") -> str:
         some_datetime = datetime.now(self.some_tz)
         return str(some_datetime.strftime(format))
 
+    def bring_raw_time(self):
+        return datetime.now(self.some_tz)
 
-@app.route('/')
-@app.route('/entry', methods=['POST'])
-def show_time() -> 'html':
-    moscow_clock = Clock('Europe/Moscow')
+
+@app.route("/")
+@app.route("/entry", methods=["POST"])
+def show_moscow_time():
+    moscow_clock = Clock("Europe/Moscow")
     moscow_time = moscow_clock.bring_time()
-    return render_template('entry.html',
-                           the_title='It is Moscow time',
+    return render_template("index.html",
+                           the_title="It is Moscow time",
                            the_time=moscow_time)
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+@app.route("/<timezone>")
+def show_user_time(timezone="Europe/Moscow"):
+    timezone = timezone.replace("-", "/")
+    try:
+        user_clock = Clock(escape(timezone))
+    except InvalidTimezoneError as err:
+        return render_template("error.html",
+                               the_title="Probably you did smth wrong",
+                               error=err
+                               )
+    user_time = user_clock.bring_time()
+    return render_template("index.html",
+                           the_title=f"It is {timezone} time",
+                           the_time=user_time)
+
+
+if __name__ == "__main__":
+    app.run(debug=False)

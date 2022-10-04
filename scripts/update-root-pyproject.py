2366413a9269b01918538fb3f8b0497276b853ac
Danila Danko <br4ch1st0chr0n3@gmail.com>
2022-10-04 14:40:46 +0300

    format

--- scripts/update-root-pyproject.py
+++ scripts/update-root-pyproject.py
@@ -4,14 +4,15 @@ from tomlkit import dumps, parse
 INPUT_FILE = sys.argv[1]
 OUTPUT_FILE = sys.argv[2]
 
-with open(INPUT_FILE, 'r') as inp:
-    with open(OUTPUT_FILE, 'r') as outr:
+with open(INPUT_FILE, "r") as inp:
+    with open(OUTPUT_FILE, "r") as outr:
         inp_toml = parse(inp.read())
         out_toml = parse(outr.read())
-        t = out_toml["tool"]["poetry"]["dev-dependencies"] | inp_toml["tool"]["poetry"]["dependencies"]
+        t = (
+            out_toml["tool"]["poetry"]["dev-dependencies"]
+            | inp_toml["tool"]["poetry"]["dependencies"]
+        )
         t.pop("python")
         out_toml["tool"]["poetry"]["dev-dependencies"] = t
-        with open(OUTPUT_FILE, 'w') as outw:
+        with open(OUTPUT_FILE, "w") as outw:
             outw.write(dumps(out_toml))
-        
-

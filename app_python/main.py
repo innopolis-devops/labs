c1bfdc7569b7a0705daceb0fecd43e7aabd12777
Danila Moriakov <d.moriakov@gmail.com>
2022-10-09 13:29:21 +0300

    app python prometheus

--- app_python/main.py
+++ app_python/main.py
@@ -1,9 +1,12 @@
 from datetime import datetime, timezone, timedelta
 from fastapi import FastAPI
 from fastapi.responses import PlainTextResponse
+from starlette_prometheus import metrics, PrometheusMiddleware
 
 
 app = FastAPI()
+app.add_middleware(PrometheusMiddleware)
+app.add_route("/metrics", metrics)
 
 
 @app.get("/", response_class=PlainTextResponse)

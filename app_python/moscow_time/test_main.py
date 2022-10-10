3e222f76205c8a0ad041d730415d1ebbae78cd70
boggda <black_sec@protonmail.com>
2022-10-11 01:40:46 +0300

    tests fix

--- app_python/moscow_time/test_main.py
+++ app_python/moscow_time/test_main.py
@@ -1,5 +1,7 @@
 from fastapi.testclient import TestClient
 from datetime import date, datetime
+import zoneinfo
+zone = zoneinfo.ZoneInfo("Europe/Moscow")
 
 from .main import app
 
@@ -19,6 +21,6 @@ def test_docs():
 def test_date():
     response = client.get("/")
     msk_time = response.json()['msk_time']
-    today = date.today()
+    today = datetime.now(zone).date()
     test_date = datetime.strptime(msk_time.split()[1], '%d.%m.%Y').date()
     assert today == test_date

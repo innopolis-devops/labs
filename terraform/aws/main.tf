ff096c83d040e8f0fe75127bc0294c87940fae46
Evgeny <afazhenya2001@gmail.com>
2022-10-11 00:16:10 +0300

    lab6

--- terraform/aws/main.tf
+++ terraform/aws/main.tf
@@ -1,23 +1,34 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.16"
-    }
-  }
-
-  required_version = ">= 1.2.0"
-}
+#terraform {
+#  required_providers {
+#    aws = {
+#      source  = "hashicorp/aws"
+#      version = "~> 4.16"
+#    }
+#  }
+#
+#  required_version = ">= 1.2.0"
+#}
+#
+#provider "aws" {
+#  region  = "eu-central-1"
+#}
+#
+#resource "aws_instance" "test" {
+#  ami           = "ami-09042b2f6d07d164a"
+#  instance_type = "t2.micro"
+#
+#  tags = {
+#    Name = "test"
+#  }
+#}
 
 provider "aws" {
-  region  = "eu-central-1"
+  region = "us-east-1"
+  access_key = ""
+  secret_key = ""
 }
 
-resource "aws_instance" "test" {
-  ami           = "ami-09042b2f6d07d164a"
+resource "aws_instance" "node_server" {
+  ami =  "ami-0f69dd1d0d03ad669"
   instance_type = "t2.micro"
-
-  tags = {
-    Name = "test"
-  }
 }

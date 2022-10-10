35e3c043f7d3a4c299f01e2f17551a7bfb7c9bf5
Evgeny <afazhenya2001@gmail.com>
2022-10-10 22:39:03 +0300

    docker tf

--- terraform/docker/main.tf
+++ terraform/docker/main.tf
@@ -1,24 +1,15 @@
-terraform {
-  required_providers {
-    docker = {
-      source  = "kreuzwerker/docker"
-      version = ">= 2.13.0"
-    }
-  }
+resource "docker_image" "my_image" {
+  name         = "afonya2001/devops:latest"
+  keep_locally = false
 }
 
 provider "docker" {
   host    = "npipe:////.//pipe//docker_engine"
 }
 
-resource "docker_image" "nginx" {
-  name         = "nginx:latest"
-  keep_locally = false
-}
-
 resource "docker_container" "nginx" {
-  image = docker_image.nginx.latest
-  name  = var.container_name
+  image = docker_image.my_image.latest
+  name  = "tutorial"
   ports {
     internal = 80
     external = 8080
--- terraform/docker/variables.tf
+++
@@ -1,5 +0,0 @@
-variable "container_name" {
-  description = "Value of the name for the Docker container"
-  type        = string
-  default     = "ExampleNginxContainer"
-}

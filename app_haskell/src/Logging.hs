c389fb8f06ed2e3af494a4334176ed648de12048
Abuzyar Tazetdinov <tazetdinov9925@gmail.com>
2022-10-16 14:57:58 +0300

    remove unnecessary Logging customizations

--- app_haskell/src/Logging.hs
+++ app_haskell/src/Logging.hs
@@ -1,86 +1,11 @@
-{-# LANGUAGE RecordWildCards #-}
-{-# LANGUAGE ViewPatterns    #-}
-
 module Logging where
 
-import qualified Chronos                    as C
 import           Colog
-import           Control.Concurrent         (ThreadId)
-import           Control.Monad.IO.Class     (MonadIO (..))
-import qualified Data.ByteString            as BS
-import qualified Data.ByteString.Char8      as BS8
-import           Data.Text                  (Text)
-import qualified Data.Text                  as T
-import           Data.Text.Encoding
-import           Data.Text.Lazy             (toStrict)
-import qualified Data.Text.Lazy.Builder     as TB
-import qualified Data.Text.Lazy.Builder.Int as TB
-import           System.IO                  (hFlush, stdout)
-
-square :: Text -> Text
-square t = "[" <> t <> "] "
-
-showThreadId :: ThreadId -> Text
-showThreadId = square . T.pack . show
-
-{- | Given a 'Datetime', constructs a 'Text' 'TB.Builder' corresponding to a
-Year\/Month\/Day,Hour\/Minute\/Second\/Offset encoding of the given 'Datetime'.
-
-Example: @2019-12-29 22:00:00.000 +00:00@
--}
-builderYMDHMSz :: C.Datetime -> TB.Builder
-builderYMDHMSz (C.Datetime date time) =
-       builderYMD date
-    <> spaceSep
-    <> C.builder_HMS (C.SubsecondPrecisionFixed 3) (Just ':') time
-    <> spaceSep
-    <> C.builderOffset C.OffsetFormatColonOn (C.Offset 0)
-  where
-    spaceSep :: TB.Builder
-    spaceSep = TB.singleton ' '
-
-    dashSep :: TB.Builder
-    dashSep = TB.singleton '-'
-
-    {- | Given a 'Date' construct a 'Text' 'TB.Builder'
-    corresponding to a Year\/Month\/Day encoding.
-
-    Example: @2020-12-31@
-    -}
-    builderYMD :: C.Date -> TB.Builder
-    builderYMD (C.Date (C.Year y) (C.Month m) (C.DayOfMonth d)) =
-           TB.decimal y
-        <> dashSep
-        <> zeroPad m
-        <> dashSep
-        <> zeroPad d
-
-    zeroPad :: Int -> TB.Builder
-    zeroPad x
-      | x < 10    = TB.singleton '0' <> TB.decimal x
-      | otherwise = TB.decimal x
-
-{-
->>> showTime $ C.Time 1577656800
-[2019-12-29 22:00:00.000 +00:00]
--}
-showTime :: C.Time -> Text
-showTime t =
-    square
-    $ toStrict
-    $ TB.toLazyText
-    $ builderYMDHMSz (C.timeToDatetime t)
-
-fmtMessageDefault :: MonadIO m => RichMessage m -> m Text
-fmtMessageDefault msg = fmtRichMessageCustomDefault msg formatRichMessage
-  where
-    formatRichMessage :: Maybe ThreadId -> Maybe C.Time -> Message -> Text
-    formatRichMessage (maybe "" showThreadId -> thread) (maybe "" showTime -> time) Msg{..} =
-        showSeverity msgSeverity
-     <> time
-     <> showSourceLoc msgStack
-     <> thread
-     <> msgText
+import           Control.Monad.IO.Class (MonadIO, liftIO)
+import qualified Data.ByteString        as BS
+import qualified Data.ByteString.Char8  as BS8
+import           Data.Text.Encoding     (encodeUtf8)
+import           System.IO              (hFlush, stdout)
 
 logByteStringStdoutFlush :: MonadIO m => LogAction m BS.ByteString
 logByteStringStdoutFlush = LogAction $ \c -> do
@@ -90,6 +15,6 @@ logByteStringStdoutFlush = LogAction $ \c -> do
 
 defaultLogAction :: MonadIO m => LogAction m Message
 defaultLogAction = upgradeMessageAction defaultFieldMap $
-    cmapM (fmap encodeUtf8 . fmtMessageDefault) logByteStringStdoutFlush
+    cmapM (fmap encodeUtf8 . fmtRichMessageDefault) logByteStringStdoutFlush
 {-# INLINE defaultLogAction #-}
 {-# SPECIALIZE defaultLogAction :: LogAction IO Message #-}

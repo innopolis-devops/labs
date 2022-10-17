53f2dcf5d05cf6d81219acd330445056fcf1efcc
Ivan Izmailov <smthngslv@duplicat.ai>
2022-10-18 00:14:53 +0400

    lab7

--- app_python/src/gunicorn.conf.py
+++ app_python/src/gunicorn.conf.py
@@ -33,8 +33,7 @@ logconfig_dict = {
     },
     'formatters': {
         'default': {
-            'format': '%(asctime)s: %(levelname)s: %(module)s: %(funcName)s: %(lineno)d:: %(message)s',
-            'datefmt': '%d-%m-%Y %H:%M:%S'
+            'format': r'%(asctime)s %(levelname)s %(module)s:%(funcName)s:%(lineno)d %(message)s',
         }
     }
 }

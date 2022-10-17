84177831a8e815eb2ad8b220647ce5d73eb5cee1
e2xen <max.ks1230@gmail.com>
2022-10-17 22:17:34 +0300

    add logging to python app

--- app_python/app/main.py
+++ app_python/app/main.py
@@ -1,11 +1,19 @@
 from datetime import datetime
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 import pytz
+import logging
+
+fmt = '%(asctime)s %(levelname)s [%(name)s] [%(funcName)s:%(lineno)s] %(message)s'
+logging.basicConfig(filename='log/main.log', encoding='utf-8', level=logging.DEBUG, format=fmt)
+logger = logging.getLogger(__name__)
 
 app = FastAPI()
 
 
 @app.get("/")
-async def get_current_time():
+async def get_current_time(request: Request):
+    ip = request.client.host
+    logger.info(f"Request from {ip}")
+
     time_zone = pytz.timezone('Europe/Moscow')
     return {"current_time": datetime.now(tz=time_zone)}

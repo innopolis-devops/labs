e408ed8ca6bbe1a90a6df4eccdb2db328f298602
Tasneem <tasneemsherif23@gmil.com>
2022-10-17 19:25:48 +0300

    add lab7

--- app_python/app.py
+++ app_python/app.py
@@ -1,11 +1,15 @@
 """
 An Application to show Current time in Moscow
 """
+import logging
 from datetime import datetime
+
 from flask import Flask
 from waitress import serve
 
 import pytz
+
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(message)s')
 
 app = Flask(__name__)
 
@@ -16,7 +20,6 @@ def index():
     timezone = pytz.timezone("Europe/Moscow")
     now = datetime.now(timezone).strftime("%H:%M:%S - %d/%m/%Y")
     return f'<h1>{str(now)}</h1>'
-
 
 if __name__ == '__main__':
     serve(app, host="0.0.0.0", port=8080)
---
+++ monitoring/LOGGING.md
@@ -0,0 +1,16 @@
+# Logging
+
+Logging is done using the PLG stack (Promtail, Loki, and Grafana).
+
+## Screenshots
+
+- Running containers in Docker Compose:
+  ![](./images/docker-containers.png)
+- Adding Loki data source:
+  ![](./images/loki-data-source.png)
+- Grafana dashboard:
+  ![](./images/python-logs.png)
+- Extra app logs:
+  ![](./images/express-logs.png)
+- Prometheus targets page:
+  ![](./images/targets.png)

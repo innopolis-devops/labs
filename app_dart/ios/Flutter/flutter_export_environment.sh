30a5e6b4867115f52f140bd31b53e0fde20bdacc
emileyray <emileyray23@gmail.com>
2022-10-17 18:03:27 +0300

    Lab 7

--- app_dart/ios/Flutter/flutter_export_environment.sh
+++ app_dart/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\src\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\Huawei\Documents\devops-labs\app_dart"
+export "FLUTTER_APPLICATION_PATH=C:\Users\Huawei\Documents\devops-lab-4\app_dart"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
---
+++ monitoring/LOGGING.md
@@ -0,0 +1,10 @@
+# Logging
+
+First, i run `sudo docker-compose up`, then my app becomes available
+
+![0.png](images/0.png)
+
+After that, on `127.0.0.1:3000`, our logs will be available
+
+![1.png](images/1.png)
+![2.png](images/2.png)

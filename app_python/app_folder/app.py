55459edb4b53d0b915b2d9da1bd04e2a651377aa
nailyav <n.valiullina@innopolis.university>
2022-10-17 21:39:23 +0300

    lab 7

--- app_python/app_folder/app.py
+++ app_python/app_folder/app.py
@@ -1,20 +1,13 @@
-from datetime import datetime
-import pytz
-import requests
+import datetime
 
+import pytz
 from flask import Flask
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def view_time():
-    url = "http://worldtimeapi.org/api/timezone/Europe/Moscow"
-    response = requests.get(url, timeout=10)
-    pattern = "%Y-%m-%dT%H:%M:%S.%f%z"
-    if response.status_code == 200:
-        date_time = datetime.strptime(response.json()["datetime"], pattern)
-    else:
-        date_time = datetime.now(pytz.timezone('Europe/Moscow'))
-    time = f"{date_time.hour}:{date_time.minute}:{date_time.second}"
-    return time
+@app.route("/")
+def moscow_time():
+    dttm = datetime.datetime.now(pytz.timezone("Europe/Moscow"))
+    time = f"{dttm.hour}:{dttm.minute}:{dttm.second}"
+    return f"<h2>Moscow time: {time}</h2>"

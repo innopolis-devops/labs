7a8a12e3f80605bf02dcfbf86118394c0d4f7d43
Dariya <dariia.vakhitova@icloud.com>
2022-10-23 13:48:06 +0300

    Lab 8

--- app_python/app.py
+++ app_python/app.py
@@ -1,13 +1,20 @@
 import pytz
 from flask import Flask
 from datetime import datetime
+from prometheus_flask_exporter import PrometheusMetrics
 
 app = Flask(__name__)
+metrics = PrometheusMetrics(app)
 
 
 @app.route('/')
 def show_time():
     return datetime.now(pytz.timezone("Europe/Moscow")).strftime("%H:%M:%S")
+
+
+@app.route("/health")
+def health():
+    return "OK"
 
 
 if __name__ == '__main__':

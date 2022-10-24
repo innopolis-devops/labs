22f4ea9830e2d0e7223a2d3ffb5c748b756b40ea
Gleb Smetanin <smetanin.glebka@gmail.com>
2022-10-25 00:42:24 +0300

    Added METRICS.md

--- app_python/main_app/main.py
+++ app_python/main_app/main.py
@@ -1,7 +1,10 @@
 from flask import Flask
+from flask import request
 import datetime
+from prometheus_flask_exporter import PrometheusMetrics
 
 app = Flask(__name__)
+metrics = PrometheusMetrics(app)
 
 @app.route("/")
 def hello_world():
@@ -9,5 +12,12 @@ def hello_world():
     now = datetime.datetime.now(moscow_time_offset)
     return "<h1>{0} : {1} : {2}</h1>".format(now.hour, now.minute, now.second)
 
+
 if __name__ == '__main__':
     app.run()
+    metrics.register_default(
+        metrics.counter(
+            'by_path_counter', 'Request count by request paths',
+            labels={'path': lambda: request.path}
+        )
+    )

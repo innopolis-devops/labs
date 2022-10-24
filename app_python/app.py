a794cdbd511958f8f0f32be70214a829686a2aac
Dariya <dariia.vakhitova@icloud.com>
2022-10-24 23:36:09 +0300

    Lab 8

--- app_python/app.py
+++ app_python/app.py
@@ -6,6 +6,8 @@ from prometheus_flask_exporter import PrometheusMetrics
 app = Flask(__name__)
 metrics = PrometheusMetrics(app)
 
+metrics.info('app_info', 'Application info', version='1.0.3')
+
 
 @app.route('/')
 def show_time():
---
+++ monitoring/METRICS.md
@@ -0,0 +1,14 @@
+1. run 
+```bash 
+docker compose up
+```
+2. go to [Link](http://localhost:9090/targets)
+
+![img_1.png](img_1.png)
+
+3. dashboard #1
+
+![img_2.png](img_2.png)
+
+4. dashboard #2
+![img_3.png](img_3.png)

f8164407c8ad436265a3954652499d3a8a722969
ilya <ilya-kolomin2001@yandex.ru>
2022-10-25 10:28:27 +0300

    feat_lab8: go metrics

--- app_go/main.go
+++ app_go/main.go
@@ -6,14 +6,34 @@ import (
 	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
+
+var requestsCounter = prometheus.NewCounter(
+   prometheus.CounterOpts{
+       Name: "app_go_request_count",
+       Help: "No of request handled",
+   },
+)
+
+func prometheusHandler() gin.HandlerFunc {
+	h := promhttp.Handler()
+
+	return func(c *gin.Context) {
+		h.ServeHTTP(c.Writer, c.Request)
+	}
+}
 
 func SetUpRouter() *gin.Engine{
 	router := gin.Default()
+	prometheus.MustRegister(requestsCounter)
 	loc := time.FixedZone("MSK", 3*60*60)
 	router.GET("/", func(c *gin.Context) {
+	    requestsCounter.Inc()
 		c.String(http.StatusOK, time.Now().In(loc).String())
 	})
+	router.GET("/metrics", prometheusHandler())
 	return router
 }
 

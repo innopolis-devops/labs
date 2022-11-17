5f1794fe395c08d3e5e0c25648840e5c278d7bb1
Kirill Ivanov <bragov4ik@hotmail.com>
2022-11-17 21:28:40 +0300

    Lab 12 base

--- app_python/src/app.py
+++ app_python/src/app.py
@@ -1,11 +1,31 @@
-from flask import Flask
+from flask import Flask, current_app
 from prometheus_flask_exporter import PrometheusMetrics
 from datetime import datetime, timedelta
 from pytz import timezone
 import requests
 import iso8601
+import json
+import pprint
+
+class Config:
+    # File to store data about visits to "/" endpoint
+    visits_file = "persistent/visits.json"
+
+
+config = Config()
+try:
+    with open("config.json", "r") as conf_file:
+        print("Reading config file")
+        config_json = json.load(conf_file)
+    expected_fields = [a for a in dir(config) if not a.startswith('__') and not callable(getattr(config, a))]
+    for f in expected_fields:
+        if f in config_json:
+            setattr(config, f, config_json[f])
+except FileNotFoundError:
+    print("Config not found, using default")
 
 app = Flask(__name__)
+app.config["CONFIG"] = config
 metrics = PrometheusMetrics(app)
 
 
@@ -44,8 +64,38 @@ def render_status_check() -> str:
 
 @app.route("/")
 def hello_world():
+    config: Config = current_app.config["CONFIG"]
+
+    # Retrieve time
     moscow_dt = local_time_moscow()
     time_string = format_time(moscow_dt, "Moscow")
+
+    # Create file if does not exist
+    import os
+    f = os.open(config.visits_file, os.O_CREAT)
+    os.close(f)
+
+    # Save page visit to file
+    with open(config.visits_file, "r+") as f:
+        try:
+            current_visits = json.load(f)
+        except json.decoder.JSONDecodeError:
+            current_visits = None
+        f.seek(0)
+        if (current_visits is None
+            or "list" not in current_visits
+            or "total" not in current_visits):
+
+            # Initialize visits file contents
+            current_visits = {
+                "list": [],
+                "total": 0
+            }
+        current_visits["list"].append(time_string)
+        current_visits["total"] += 1
+        json.dump(current_visits, f)
+        f.truncate()
+
     return render_time_page(time_string)
 
 
@@ -56,3 +106,17 @@ def status_check():
     except Exception as e:
         app.logger.warn("Could not do status check: %s", e)
         return "Failed to perform status check", 503
+
+@app.route("/visits")
+def list_visits():
+    config: Config = current_app.config["CONFIG"]
+    try:
+        with open(config.visits_file, "r") as f:
+            v = json.load(f)
+            result = "Total visits: " + str(v["total"]) + "<br>"
+            result += "Entries:<br>"
+            for entry in v["list"]:
+                result += str(entry) + "<br>"
+            return result
+    except (FileNotFoundError, json.decoder.JSONDecodeError):
+        return "No visits to \"/\" detected"

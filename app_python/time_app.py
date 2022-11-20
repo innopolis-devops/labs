e2f78842e8cbb725b181008791359c2d770e2fe7
Mikhail <morser1243@gmail.com>
2022-11-20 18:20:33 +0300

    lab12

--- app_python/time_app.py
+++ app_python/time_app.py
@@ -5,6 +5,8 @@ from flask import Flask
 from flask import request
 from healthcheck import HealthCheck
 from prometheus_flask_exporter import PrometheusMetrics
+import json
+import os
 
 app = Flask(__name__)
 metrics = PrometheusMetrics(app)
@@ -12,6 +14,8 @@ metrics = PrometheusMetrics(app)
 health = HealthCheck()
 
 time_start = datetime.now()
+
+VISITS_FILE = "/home/app/visits.json"
 
 
 @app.route('/', methods=['GET'])
@@ -28,7 +32,49 @@ def time():
          """
     moscow = pytz.timezone('Europe/Moscow')
     time_format = '%Y-%m-%d %H:%M:%S %Z%z'
+    saveVisit()
     return content % datetime.now(moscow).strftime(time_format)
+
+
+@app.route('/visits', methods=['GET'])
+def visits():
+    checkExists()
+    with open(VISITS_FILE, "r") as f:
+        v = json.load(f)
+        res = "Total " + str(v["totalVisits"]) + " <br>"
+        res += "Entries: <br>"
+        for entry in v["visitsList"]:
+            res += str(entry) + "<br>"
+        return res
+
+
+def saveVisit():
+    checkExists()
+
+    current_state = {}
+    with open(VISITS_FILE, "r") as fr:
+        current_state = json.load(fr)
+
+        moscow = pytz.timezone('Europe/Moscow')
+        time_format = '%Y-%m-%d %H:%M:%S %Z%z'
+
+        current_state["visitsList"].append(datetime.now(moscow).strftime(time_format))
+        current_state["totalVisits"] += 1
+    with open(VISITS_FILE, "w") as fw:
+        json.dump(current_state, fw)
+
+
+def checkExists():
+    if not os.path.exists(VISITS_FILE):
+        print("crate file")
+        f = os.open(VISITS_FILE, os.O_CREAT)
+        os.close(f)
+
+        with open(VISITS_FILE, "w") as file:
+            print("crate entries")
+            visitsMap = {"visitsList": [], "totalVisits": 0}
+            json.dump(visitsMap, file)
+
 
 
 def app_available():

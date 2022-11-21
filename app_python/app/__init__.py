f533fc919d89e7c0d4274fc44d34711a2bd62e2d
zZzwat4er <valenten2001@mail.ru>
2022-11-21 20:32:14 +0300

    lab12

--- app_python/app/__init__.py
+++ app_python/app/__init__.py
@@ -2,6 +2,9 @@ from flask import Flask
 from datetime import datetime
 from prometheus_flask_exporter import PrometheusMetrics
 import pytz
+from pathlib import Path
+
+Path("./volume").mkdir(exist_ok=True)
 
 app = Flask(__name__)
 metrics = PrometheusMetrics(app)
@@ -11,4 +14,22 @@ metrics.info('app_info', 'Application info', version='1.0.3')
 
 @app.route('/')
 def index():
+    try:
+        with open("./volume/visits", "r") as f:
+            visits = int(f.read())
+    except FileNotFoundError:
+        visits = 0
+    with open("./volume/visits", "w") as f:
+        f.write(str(visits + 1))
     return f'{datetime.now(pytz.timezone("Europe/Moscow"))}'
+
+
+@app.route('/v')
+def asdf():
+    try:
+        with open("./volume/visits", "r") as f:
+            visits = int(f.read())
+    except FileNotFoundError:
+        visits = 0
+
+    return str(visits)

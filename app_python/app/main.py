417ac65a67ba5a1ff33c8c33089f9b5894adde66
SMore-Napi <s.roma245@gmail.com>
2022-11-21 22:35:00 +0300

    Lab 12. Roman Soldatov

--- app_python/app/main.py
+++ app_python/app/main.py
@@ -1,7 +1,9 @@
 """Server module."""
+import json
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
 from fastapi import Response
+# pylint: disable=import-error
 from starlette_prometheus import metrics, PrometheusMiddleware
 # pylint: disable=import-error
 from view import html_response
@@ -18,6 +20,15 @@ async def get_msk_time():
     """Get request for the main page"""
     zone = "Moscow"
     time = msk_time()
+    with open('data/visits.json', encoding="utf-8") as json_file:
+        data = json.load(json_file)
+        data['total_number'] = int(data['total_number']) + 1
+        visit_times = data['visits']
+        visit_times.append(time)
+        data['visits'] = visit_times
+        json_string = json.dumps(data)
+        with open('data/visits.json', 'w', encoding="utf-8") as outfile:
+            outfile.write(json_string)
     return html_response(zone, time)
 
 
@@ -25,3 +36,11 @@ async def get_msk_time():
 async def healthcheck():
     """Get healthcheck"""
     return Response(content='{"status":"UP"}', media_type="application/json")
+
+
+@app.get("/visits")
+async def visits():
+    """Get visits"""
+    with open('data/visits.json', encoding="utf-8") as json_file:
+        json_string = json.dumps(json.load(json_file))
+        return Response(content=json_string, media_type="application/json")

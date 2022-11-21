f21edaf8ace81eb4d0569d21ad92f26859e58f4a
AKurmazov <akurmazov2001@gmail.com>
2022-11-21 22:54:06 +0300

    Complete main task

--- app_python/server/router.py
+++ app_python/server/router.py
@@ -5,6 +5,7 @@ from fastapi import APIRouter
 from fastapi.responses import HTMLResponse
 
 from .config import config
+from .utils import increment_visits, current_visits
 
 tz = pytz.timezone(config.TIMEZONE)
 
@@ -12,5 +13,11 @@ router = APIRouter()
 
 
 @router.get('/', response_class=HTMLResponse)
+@increment_visits
 async def get_current_time():
     return f'<h1>{datetime.now(tz)}</h1>'
+
+
+@router.get('/visits', response_class=HTMLResponse)
+async def get_visits():
+    return f'<h1>Visits: {await current_visits()}'

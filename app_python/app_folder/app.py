d3d0367a6a346a4ed52e6a1ce4d3108b936eb7a7
nailyav <n.valiullina@innopolis.university>
2022-11-22 08:31:28 +0300

    lab 12 done

--- app_python/app_folder/app.py
+++ app_python/app_folder/app.py
@@ -1,7 +1,8 @@
 import datetime
+import os
 
 import pytz
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -10,4 +11,23 @@ app = Flask(__name__)
 def moscow_time():
     dttm = datetime.datetime.now(pytz.timezone("Europe/Moscow"))
     time = f"{dttm.hour}:{dttm.minute}:{dttm.second}"
+    if not os.path.exists('/home/app/data/visits.txt'):
+        print('ERROR: visits file not found')
+        return f"<h2>Moscow time: {time}</h2>"
+
+    with open("/home/app/data/visits.txt", "a+") as f:
+        f.write(f"{dttm.isoformat(timespec='seconds')} - {request.remote_addr}\n")
     return f"<h2>Moscow time: {time}</h2>"
+
+
+@app.route('/visits')
+def visits():
+    web_content = "History:\n"
+    try:
+        with open("/home/app/data/visits.txt", "r") as fo:
+            file_text = fo.read()
+            web_content += f"{file_text}"
+    except FileNotFoundError:
+        pass
+
+    return web_content

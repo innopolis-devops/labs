512b61bf9a617e0a7ac5551ae62ae52961cd9e9a
Vladimir Markov <Markovvn1@gmail.com>
2022-11-22 03:33:46 +0300

    Complete 5th task

--- app_python/superclock/app.py
+++ app_python/superclock/app.py
@@ -1,7 +1,11 @@
+from typing import List
+import datetime
+
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse, Response
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from pydantic import BaseModel
 from prometheus_fastapi_instrumentator import Instrumentator
 
 from .metrics import http_requested_languages_total
@@ -15,9 +19,27 @@ templates = Jinja2Templates(directory=PROJECT_PATH / "superclock/templates")
 msk_clock = TimezoneClock("Europe/Moscow")
 
 
+class VisitorInfo(BaseModel):
+    requested_page: str
+    client: str
+    time: datetime.datetime
+
+
 @app.get("/", response_class=HTMLResponse)
 async def home(request: Request) -> Response:
+    with open("visits.txt", "a+", encoding="utf8") as f:
+        client = request.client.host if request.client is not None else None
+        f.write(VisitorInfo(requested_page="/", client=client, time=msk_clock.get_datetime()).json() + "\n")
     return templates.TemplateResponse("index.html", {"request": request, "msktime": msk_clock.get_time_str()})
+
+
+@app.get("/visits")
+async def visits() -> List[str]:
+    try:
+        with open("visits.txt", "rb") as f:
+            return [VisitorInfo.parse_raw(i) for i in f.read().split(b"\n")[:-1]]
+    except FileNotFoundError:
+        return []
 
 
 @app.get("/healthy")

cbcafc084ecc3af5aad92eba2a12d41cebcecf1e
Arina Drenyassova <a.drenyasova@innopolis.university>
2022-11-23 15:21:30 +0300

    lab12

--- app_python/moscow_time/main.py
+++ app_python/moscow_time/main.py
@@ -1,8 +1,12 @@
 """This module shows Moscow Time."""
 from datetime import datetime
 from flask import Flask
+import os
 
 import requests
+
+os.makedirs('volume', exist_ok=True)
+visits_file = 'volume/visits.txt'
 
 app = Flask(__name__)
 
@@ -15,7 +19,19 @@ def moscow_time():
     if response.status_code == 200:
         date = datetime.strptime(response.json()['datetime'], pattern)
         time = f"{date.hour}:{date.minute}:{date.second}"
+
+    with open(visits_file, 'a') as file:
+        file.write(date + ' ' + time + '\n')
+
     return f"Moscow time: {time}"
+
+@app.route("/visits")
+def visits():
+
+    with open(visits_file, 'r') as file:
+        visits = file.read()
+
+    return visits
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')

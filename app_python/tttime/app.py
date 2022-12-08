52003928454637a37050c1aa3222a85b811c480f
Roman Nabiullin <nabiul_r@modulotech.fr>
2022-12-08 16:00:25 +0300

    Updates.

--- app_python/tttime/app.py
+++ app_python/tttime/app.py
@@ -1,10 +1,29 @@
-import os
+import json
+from flask import jsonify
 from flask import Flask
 from flask import render_template
 from datetime import datetime
 from pytz import timezone
 from prometheus_flask_exporter import PrometheusMetrics
-from visit_tracker import VisitTracker
+
+
+class VisitTracker:
+
+    def __init__(self, filename: str) -> None:
+        self.filename = filename
+
+    def increase_number_of_visits(self):
+        number_of_visits = 0
+        with open(self.filename, mode='r') as file:
+            number_of_visits = json.load(file)
+        with open(self.filename, mode='w') as file:
+            json.dump(number_of_visits + 1, file)
+
+    def get_visits_as_json(self):
+        number_of_visits = 0
+        with open(self.filename, mode='r') as file:
+            number_of_visits = json.load(file)
+        return number_of_visits
 
 
 def make_app():

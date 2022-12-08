43d3579cbab7ebee12693bcb6c48df24839bb1f3
Roman Nabiullin <nabiul_r@modulotech.fr>
2022-12-08 15:22:39 +0300

    Update app.

--- app_python/tttime/app.py
+++ app_python/tttime/app.py
@@ -1,8 +1,10 @@
+import os
 from flask import Flask
 from flask import render_template
 from datetime import datetime
 from pytz import timezone
 from prometheus_flask_exporter import PrometheusMetrics
+from visit_tracker import VisitTracker
 
 
 def make_app():
@@ -11,12 +13,18 @@ def make_app():
     """
     app = Flask(__name__, template_folder="../templates")
     metrics = PrometheusMetrics(app)
+    visit_tracker = VisitTracker(filename='data/visits.json')
 
     @app.route('/', methods=['GET'])
     def index():
+        visit_tracker.increase_number_of_visits()
         zone = timezone('Europe/Moscow')
         time = datetime.now(zone).strftime('%H:%M')
         return render_template('main.html', time=time)
+
+    @app.route('/visits', methods=['GET'])
+    def visits():
+        return render_template('visits.html', visits=visit_tracker.get_visits_as_json())
 
     return app
 

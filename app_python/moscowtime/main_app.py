4b2048c1b1b377664b5801270e6e69190f67563b
Nurdaulet Kunkhozhaev <n.kunkhozhaev@gmail.com>
2022-12-09 16:39:46 +0300

    Added unit tests

--- app_python/moscowtime/main_app.py
+++ app_python/moscowtime/main_app.py
@@ -5,12 +5,14 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def moscow_time():
     t_zone = pytz.timezone('Europe/Moscow')
     time_now = datetime.now(t_zone)  # current date and time in timezone = t_zone
     formatted_time = time_now.strftime("%H:%M:%S")
     return render_template("moscow_timezone.html", time=formatted_time)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')

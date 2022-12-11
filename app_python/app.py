6b82af5c250afa496143c403149e60c581ab4cb0
ilsur <IfhnlbyjdBkmceh@yandex.ru>
2022-12-11 18:42:52 +0300

    changed python app

--- app_python/app.py
+++ app_python/app.py
@@ -1,13 +1,30 @@
 from flask import Flask
+import os
 import datetime
 
 app = Flask(__name__)
+
+log_filename = os.getenv("LOG_FILE_ENV", "data/visits.log")
+os.makedirs(os.path.dirname(log_filename), exist_ok=True)
+
+
+def save_time(current_time):
+    with open(log_filename, "a+") as f:
+        f.write(current_time)
+        f.write("\n")
 
 
 @app.route('/')
 def show_time():
     moscow_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    save_time(moscow_time)
     return moscow_time
+
+
+@app.route("/visits")
+def get_visits():
+    with open(log_filename, "r") as f:
+        return "".join(f.readlines())
 
 
 if __name__ == '__main__':

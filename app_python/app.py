b0b23bcd07d63d7f9c7442683c2cf40abcbe11fa
romanmukhtarov <r.mukhtarov@innopolis.university>
2022-12-12 03:17:28 +0300

    lab12

--- app_python/app.py
+++ app_python/app.py
@@ -5,14 +5,29 @@ from prometheus_flask_exporter import PrometheusMetrics
 
 app = Flask(__name__)
 metrics = PrometheusMetrics(app)
+visits_path = './time_visit.txt'
 
+def write_visit(date):
+    with open(visits_path, 'a') as visits_file:
+        visits_file.write(f"{str(date)}\n")
 @app.route('/')
 def show_moscow_time():
     offset = datetime.timezone(datetime.timedelta(hours=3))
     moscow_time = datetime.datetime.now(offset)
     print(type(moscow_time))
-    return render_template('moscow_time.html', moscow_time=moscow_time, time=moscow_time.strftime("%H:%M:%S"))
+    clear_date = moscow_time.strftime("%H:%M:%S")
+    write_visit(clear_date)
+    return render_template('moscow_time.html', moscow_time=moscow_time, time=clear_date)
 
+
+@app.route('/visits')
+def visits():
+    visits= []
+    with open(visits_path, 'r') as visitsFile:
+        lines = visitsFile.readlines()
+        for line in lines:
+            visits.append(line[:-2])
+    return visits
 
 if __name__ == '__main__':
     app.run()

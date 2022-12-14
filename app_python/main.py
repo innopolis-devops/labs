d2ccbe280d3273225a89978585c9e774ce86fc41
Aleksandr Krotov <san.crot@yandex.ru>
2022-12-14 08:08:45 +0300

    Lab12

--- app_python/main.py
+++ app_python/main.py
@@ -6,13 +6,33 @@ app = FastAPI()
 mem = "mem"
 
 
+def get_visits():
+    try:
+        with open('./volume/visits.txt', 'r') as f:
+            return int(f.read())
+    except FileNotFoundError:
+        return 0
+
+
+def inc_visits():
+    data = str(get_visits() + 1)
+    with open('./volume/visits.txt', 'w') as f:
+        f.write(data)
+
+
 def get_msk_time():
     return datetime.datetime.now().astimezone(timezone("Europe/Moscow"))
 
 
 @app.get("/")
 def read_root():
+    inc_visits()
     return {"msktime": get_msk_time().strftime("%H:%M:%S")}
+
+
+@app.get("/visits")
+def read_root():
+    return {"visits": get_visits()}
 
 
 @app.post("/outofmemory")

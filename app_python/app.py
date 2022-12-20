2da61c312c7bf27b3f312bbe006144034310ed08
KsEv13 <eksenrus@gmail.com>
2022-12-20 03:53:08 +0300

    Lab 12. K8s ConfigMaps

--- app_python/app.py
+++ app_python/app.py
@@ -4,20 +4,30 @@ from datetime import datetime
 from flask import Flask
 from flask import render_template
 from pytz import timezone
+import os
 
 app = Flask(__name__)
 app.static_folder = 'static'
 
+os.makedirs('volume', exist_ok=True)
+visits_file = 'volume/visits.txt'
 
 @app.route("/")
 def render_time_page():
-    return render_template(
-        "time.html",
-        date=datetime.now(timezone('Europe/Moscow'))
-    )
+    _date=datetime.now(timezone('Europe/Moscow'))
+    with open(visits_file, 'a') as file:
+        file.write(str(_date))
+    return render_template("time.html", date = _date)
+
 @app.route("/health")
 def health():
     return "200"
+
+@app.route("/visits")
+def visits():
+    with open(visits_file, 'r') as file:
+        visits = file.read()
+    return visits
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=8000, debug=True)

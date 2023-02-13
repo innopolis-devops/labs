a2589bc2b59ebb6fbc2fb71a703937bc79aa5504
NurbakZh <mineman135@gmail.com>
2023-02-13 17:47:56 +0300

    done

---
+++ monitoring/METRICS.md
@@ -0,0 +1,10 @@
+## Screenshots of success:
+___
+#### Metrics
+![metrics](images/metrics.png)
+
+#### Prometeheus:
+![label filter](images/prometheus.png)
+
+#### Loki:
+![label filter](images/loki.png)
---
+++ terraform/GitHubTerraForm/Main.tf
@@ -0,0 +1,40 @@
+terraform {
+  required_providers {
+    github = {
+      source  = "integrations/github"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "github" {
+  token = var.token
+}
+
+resource "github_repository" "repo" {
+  name               = "DevOps_terraGit"
+  description        = "Repo created by terraform"
+  visibility         = "public"
+  has_issues         = true
+  has_wiki           = true
+  auto_init          = true
+  license_template   = "mit"
+}
+
+#Set default branch 'master'
+resource "github_branch_default" "master" {
+  repository = github_repository.repo.name
+  branch     = "master"
+}
+
+#Create branch protection rule to protect the default branch. (Use "github_branch_protection_v3" resource for Organisation rules)
+resource "github_branch_protection" "default" {
+  repository_id                   = github_repository.repo.id
+  pattern                         = github_branch_default.master.branch
+  require_conversation_resolution = true
+  enforce_admins                  = true
+
+  required_pull_request_reviews {
+    required_approving_review_count = 1
+  }
+}

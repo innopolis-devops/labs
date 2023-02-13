58e4f021ee425356e31890955501fa20fe47beee
NurbakZh <mineman135@gmail.com>
2023-02-13 15:30:15 +0300

    done Lab4

---
+++ terraform/Outputs.tf
@@ -0,0 +1,10 @@
+output "container_id" {
+  description = "ID of the Docker container"
+  value       = docker_container.nginx.id
+}
+
+output "image_id" {
+  description = "ID of the Docker image"
+  value       = docker_image.nginx.id
+}
+

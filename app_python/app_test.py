a55b9c887a622c4661d1cface51194e371946635
Yelshat Duskaliyev <ura2178@gmail.com>
2023-02-14 05:18:03 +0300

    feat: added python workflow and test

---
+++ app_python/app_test.py
@@ -0,0 +1,34 @@
+import pytest
+from flask.testing import FlaskClient
+
+from app import app
+
+client: FlaskClient = app.test_client()
+
+
+# freeze time to 2020-01-01 00:00:00
+@pytest.fixture(autouse=True)
+def freeze_time(monkeypatch):
+    from freezegun import freeze_time
+
+    with freeze_time('2020-01-01 00:00:00', tz_offset=0):
+        yield
+
+
+EXPECTED_RESPONSE = b'''
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Lab 1</title>
+</head>
+<body>
+    <h1>Current time: 00:00:00</h1>
+</body>
+</html>'''
+
+
+def test_show_time():
+    response = client.get('/')
+    assert response.status_code == 200
+    assert response.data == EXPECTED_RESPONSE
+    assert response.headers['Content-Type'] == 'text/html; charset=utf-8'

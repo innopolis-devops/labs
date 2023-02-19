fab15b8d4721dce9ae50e2522e5adaa29500144d
dapp-anyway <dapp.anyway@gmail.com>
2023-02-19 21:17:22 +0300

    lab13__

--- app_python/app.py
+++ app_python/app.py
@@ -7,8 +7,17 @@ time = "%d.%m.%Y %H:%M:%S"
 
 app = Flask(__name__)
 
+VOLUME = "volume/visits.txt"
+
 
 @app.route("/")
 def get_time():
     moscow_time = datetime.now(pytz.timezone(time_zone)).strftime(time)
     return '<h1>' + moscow_time + '</h1>'
+
+
+@app.route("/visits")
+def get_visits():
+    with open(VOLUME, 'r') as f:
+        visits = f.readlines()
+    return '\n'.join(visits)

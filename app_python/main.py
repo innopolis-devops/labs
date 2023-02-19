50d1a504695ad8235d482719cbb99fe154cb476e
TonyDecvA180XN <tony.dospiekhov@gmail.com>
2023-02-19 21:34:48 +0300

    Add Config Maps

--- app_python/main.py
+++ app_python/main.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from prometheus_flask_exporter import PrometheusMetrics
 
+import os
 import time_utils
 
 # flask server entry point
@@ -10,6 +11,14 @@ metrics = PrometheusMetrics(app)
 # index page callback
 @app.route("/")
 def default_page():
+    if os.path.exists("count.txt"):
+        with open("count.txt", "r") as file:
+            count = int(file.read())
+    else:
+        count = 0
+    with open("count.txt", "w") as file:
+        file.write(str(count + 1))
+        
     return """
     <html>
         <head>
@@ -22,3 +31,23 @@ def default_page():
         </body>
     </html>
     """.format(time_utils.get_time_in_moscow())
+
+@app.route("/visits")
+def popularity():
+    if os.path.exists("count.txt"):
+        with open("count.txt", "r") as file:
+            count = int(file.read())
+    else:
+        count = 0
+    return """
+    <html>
+        <head>
+            <style>
+                h1 {{text-align: center;}}
+            </style>
+        </head>
+        <body>
+            <h1>Time was requested times: {}</h1>
+        </body>
+    </html>
+    """.format(count)

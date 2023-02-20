2f3964be6692ffdf8506bfa83ea6030bb8b739b5
Ilya <nechesanov2000@gmail.com>
2023-02-20 20:32:17 +0300

    lab13

--- app_python/app.py
+++ app_python/app.py
@@ -1,11 +1,42 @@
-from flask import Flask
-from datetime import datetime
-from pytz import timezone
+from flask import Flask, render_template, request
+import datetime
+from prometheus_flask_exporter import PrometheusMetrics
 
 app = Flask(__name__)
+metrics = PrometheusMetrics(app)
+visits_path = './time_visit.txt'
+
+
+def write_visit(date):
+    with open(visits_path, 'a') as visits_file:
+        visits_file.write(f"{str(date)}\n")
 
 
 @app.route('/')
-def hello():
-    tz = timezone('Europe/Moscow')
-    return 'Now in Moscow: ' + datetime.now(tz).strftime('%d-%m-%Y %H:%M:%S')
+def show_moscow_time():
+    offset = datetime.timezone(datetime.timedelta(hours=3))
+    moscow_time = datetime.datetime.now(offset)
+    print(type(moscow_time))
+    clear_date = moscow_time.strftime("%H:%M:%S")
+    write_visit(clear_date)
+    return render_template('moscow_time.html', moscow_time=moscow_time, time=clear_date)
+
+
+@app.route('/visits')
+def visits():
+    visits = []
+    with open(visits_path, 'r') as visitsFile:
+        lines = visitsFile.readlines()
+        for line in lines:
+            visits.append(line[:-2])
+    return visits
+
+
+if __name__ == '__main__':
+    app.run()
+    metrics.register_default(
+        metrics.counter(
+            'by_path_counter', 'Request count by request paths',
+            labels={'path': lambda: request.path}
+        )
+    )

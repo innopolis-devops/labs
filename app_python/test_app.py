01603f34405a39ec06da7fb9362d07c8724c6eea
Ilya <nechesanov2000@gmail.com>
2023-02-20 21:05:08 +0300

    fix

--- app_python/test_app.py
+++ app_python/test_app.py
@@ -1,8 +1,30 @@
-from config_test import client
+def write_visit(date):
+    with open(visits_path, 'a') as visits_file:
+        visits_file.write(f"{str(date)}\n")
+@app.route('/')
+def show_moscow_time():
+    offset = datetime.timezone(datetime.timedelta(hours=3))
+    moscow_time = datetime.datetime.now(offset)
+    print(type(moscow_time))
+    clear_date = moscow_time.strftime("%H:%M:%S")
+    write_visit(clear_date)
+    return render_template('moscow_time.html', moscow_time=moscow_time, time=clear_date)
 
-def test_request_to_existing_url():
-    assert client.get('/').status_code == 200
 
+@app.route('/visits')
+def visits():
+    visits= []
+    with open(visits_path, 'r') as visitsFile:
+        lines = visitsFile.readlines()
+        for line in lines:
+            visits.append(line[:-2])
+    return visits
 
-def test_request_to_not_existing_url():
-    assert client.get('/test').status_code == 404
+if __name__ == '__main__':
+    app.run()
+    metrics.register_default(
+        metrics.counter(
+            'by_path_counter', 'Request count by request paths',
+            labels={'path': lambda: request.path}
+        )
+    )
